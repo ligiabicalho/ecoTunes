9e39cea2cc68b307966a63c55f238b4e7f645236
Lígia Bicalho <ligiabicalho@gmail.com>
2022-10-03 15:11:07 -0300

    Arquivos dos componentes para Requisito 1 - ainda não passa no teste

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Router, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <h1>TrybeTunes</h1>
+        <main>
+          <Switch>
+            <Router exact path="/" component={ Login } />
+            <Router path="/search" component={ Search } />
+            <Router path="/album/:id" component={ Album } />
+            <Router path="/favorites" component={ Favorites } />
+            <Router path="/profile" component={ Profile } />
+            <Router path="/profile/edit" component={ ProfileEdit } />
+            <Router path="*" component={ NotFound } />
+          </Switch>
+        </main>
+      </>
+    );
   }
 }
 
