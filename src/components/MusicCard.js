4b7762ec33e3ff54ac80056d0427f3c69b46d254
Lígia Bicalho <ligiabicalho@gmail.com>
2022-10-04 17:41:33 -0300

    Requisito 7

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -9,23 +9,14 @@ class MusicCard extends React.Component {
         { musics.map((music, i) => (
           i === 0
             ? (
-              <li key={ i }>
+              <div key={ i }>
                 <h1 data-testid="album-name">{music.collectionName}</h1>
                 <h2 data-testid="artist-name">{music.artistName}</h2>
-                <audio data-testid="audio-component">
-                  {/* Tentanto enganar o test para validar o Requisito */}
-                  <track kind="captions" />
-                  {music.trackName}
-                </audio>
-              </li>
+              </div>
             )
             : (
               <li key={ i }>
-                <p>
-                  Música:
-                  {' '}
-                  {music.trackName}
-                </p>
+                {music.trackName}
                 <audio data-testid="audio-component" src={ music.previewUrl } controls>
                   <track kind="captions" />
                   O seu navegador não suporta o elemento
