d7d35d1a91e010d09cb6128d4fd40ed8b5b98ea3
Lígia Bicalho <ligiabicalho@gmail.com>
2022-10-04 10:59:15 -0300

    Requisito 6 - em andamento - componente Card atualizado

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,43 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Card extends React.Component {
-  state = {
-    resultState:,
-  }
-  componentDidMount(){
-    <
-  }
   render() {
     const { resultados } = this.props;
-    const { resultState } = this.state;
     return (
-      // { resultados === [] 
-      //   ? <div>Nenhum álbum foi encontrado </div>
-      //   : (
-        <div className="card-search-result">
-            <h3>
-              Resultado de álbuns de:
-              {resultados[0].artistName}
-            </h3>
-            <div>
-              {resultados.map((item) => (
-                <div key={ item.collectionId }>
-                  Artista:
-                  {item.artistName}
-                  <br />
-                  Album:
-                  {item.collectionName}
-                  <br />
-                  <img src={ item.artworkUrl100 } alt={ item.artistName } />
-                </div>
-              ))}
+      <div className="card-search-result">
+        <h3>
+          Resultado de álbuns de:
+          {resultados[0].artistName}
+        </h3>
+        <div>
+          {resultados.map((item) => (
+            <div key={ item.collectionId }>
+              Artista:
+              {item.artistName}
+              <br />
+              Album:
+              {item.collectionName}
+              <br />
+              <img src={ item.artworkUrl100 } alt={ item.artistName } />
             </div>
-          </div>
-          ) 
-      }
-    // );
+          ))}
+        </div>
+      </div>
+    );
   }
-// }
+}
+
 Card.propTypes = {
   resultados: PropTypes.arrayOf,
 }.isRequired;
